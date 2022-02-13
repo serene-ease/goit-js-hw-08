e05efebb7c03482b90ebaa387737b46c1bf116c7
Ivan <89925433+serene-ease@users.noreply.github.com>
2022-02-13 17:02:17 +0200

    look for bags

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,18 @@
-import throttle from 'lodash.throttle';
+import throttle from 'lodash/throttle';
 
 const playerMarkup = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(playerMarkup);
 const VIDEO_BEGIN_POSITION = 'video-time-point';
 const playedSeconds = JSON.parse(localStorage.getItem(VIDEO_BEGIN_POSITION));
+// const playedSeconds = localStorage.getItem(VIDEO_BEGIN_POSITION);
 
 
-
-player.on('timeupdate', throttle((currentTime => 
-    {
-        localStorage.setItem(VIDEO_BEGIN_POSITION, JSON.stringify(currentTime));
-    }
-    ),
-    1000));
+player.setCurrentTime(playedSeconds);
 
 
-if (playedSeconds === 0)
-{
-player.setCurrentTime(playedSeconds.seconds);
-}
+player.on('timeupdate', throttle(function (currentTime) {
+    localStorage.setItem(VIDEO_BEGIN_POSITION, JSON.stringify(currentTime.seconds));
+    // localStorage.setItem(VIDEO_BEGIN_POSITION, currentTime.seconds);
+}, 1000));
+
+
